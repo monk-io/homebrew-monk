8156ac58efc3b3dfbf90918d45d55d3d087677fe
Circle CI job <cicd@monk.io>
2021-04-16 13:45:35 +0000

    updated for v3.0.9

--- monk.rb
+++ monk.rb
@@ -1,9 +1,9 @@
 class Monk < Formula
   desc "Monk is the stack orchestration platform. K8s alternative."
   homepage "https://monk.io"
-  url "https://get.monk.io/stable/macos/monk-darwin-v3.0.8.tar.gz"
-  sha256 "211303934c0259c1d9e8a365fd08fc80831e259650bf07a4f8fd5d115e51db7d"
-  version "3.0.8"
+  url "https://get.monk.io/stable/macos/monk-darwin-v3.0.9.tar.gz"
+  sha256 "d926bd884fa87314f50642594ab25d374201ccd7162f7740c7d69b90eda5ecce"
+  version "3.0.9"
 
   bottle :unneeded
 
