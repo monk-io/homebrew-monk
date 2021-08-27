27276da5c175a1dcb35d2a46c72270925ace21ec
Circle CI job <cicd@monk.io>
2021-08-27 13:58:08 +0000

    updated for v3.1.10

--- monk.rb
+++ monk.rb
@@ -1,9 +1,9 @@
 class Monk < Formula
   desc "Monk is the stack orchestration platform. K8s alternative."
   homepage "https://monk.io"
-  url "https://get.monk.io/stable/macos/monk-darwin-v3.1.9.tar.gz"
-  sha256 "84ec3bec27a8ab37df3255375441e8d2b18746bd74b294b65760aad174f4da25"
-  version "3.1.9"
+  url "https://get.monk.io/stable/macos/monk-darwin-v3.1.10.tar.gz"
+  sha256 "0bf92c80d59e7a67119fb8c7e3f326f18ad1f58d868e21d2b3d28dfc375107bb"
+  version "3.1.10"
 
   bottle :unneeded
 
@@ -11,4 +11,35 @@ class Monk < Formula
     bin.install "monk" => "monk"
     bin.install "monkd" => "monkd"
   end
+
+
+  plist_options manual: "monkd"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>EnableGlobbing</key>
+          <true/>
+          <key>ProgramArguments</key>
+          <array>
+            <string>/usr/local/bin/monkd</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>/tmp/com.monkd.daemon.plist.error.log</string>
+          <key>StandardOutPath</key>
+          <string>/tmp/com.monkd.daemon.plist.log</string>
+          <key>UserName</key>
+          <string>{{USERNAME}}</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
 end
