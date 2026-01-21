78a569d0ac47eb9adf8c814f66ed0292b2a3f5db
Circle CI job <cicd@monk.io>
2026-01-21 19:19:58 +0000

    updated for v3.18.9

--- monk.rb
+++ monk.rb
@@ -1,12 +1,12 @@
 class Monk < Formula
   desc "Monk is the stack orchestration platform. K8s alternative."
   homepage "https://monk.io"
-  version "3.18.8"
+  version "3.18.9"
 
-  url_x64 = "https://get.monk.io/stable/macos/monk-darwin-v3.18.8.tar.gz"
-  sha256_x64 = "d5d97d86e96d6109438239b9b103dce4d0a579f5a9912e449a1549f91eca108f"
-  url_arm64 = "https://get.monk.io/stable/macos/monk-arm-darwin-v3.18.8.tar.gz"
-  sha256_arm64 = "a7949b1c3e685f610b8cdfec067989fe2de5384907c3cdd4b1929822eaf7b708"
+  url_x64 = "https://get.monk.io/stable/macos/monk-darwin-v3.18.9.tar.gz"
+  sha256_x64 = "60aa173f0e6e5ecf1fc48b6f63109c2e04cb87ad8178c9ae992e2089e3298613"
+  url_arm64 = "https://get.monk.io/stable/macos/monk-arm-darwin-v3.18.9.tar.gz"
+  sha256_arm64 = "20846550910d0d946fad7fdba5de079d6dac2e1efab2b0b3375991c9eca8b147"
 
   if Hardware::CPU.intel?
     sha256 sha256_x64
