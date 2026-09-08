1171ca488c1ec4c6289544844a4625c41532fe06
monk-io CI <cicd@monk.io>
2026-09-08 10:49:48 +0000

    updated for v3.21.4

--- monk.rb
+++ monk.rb
@@ -1,12 +1,12 @@
 class Monk < Formula
   desc "Monk is the stack orchestration platform. K8s alternative."
   homepage "https://monk.io"
-  version "3.21.3"
+  version "3.21.4"
 
-  url_x64 = "https://get.monk.io/stable/macos/monk-darwin-v3.21.3.tar.gz"
-  sha256_x64 = "6f3485226a29c42f53d90b8fe94d12bbc5336651dca4f5bc62e065e013a100d1"
-  url_arm64 = "https://get.monk.io/stable/macos/monk-arm-darwin-v3.21.3.tar.gz"
-  sha256_arm64 = "62a966244f6909177a6f59b503fa379d9023d39444f38e06e7eca395f1edd7e6"
+  url_x64 = "https://get.monk.io/stable/macos/monk-darwin-v3.21.4.tar.gz"
+  sha256_x64 = "7c044a877c9d99156f0eb8212ba54147ff8000148b090b197dc61b2a6cbf1e09"
+  url_arm64 = "https://get.monk.io/stable/macos/monk-arm-darwin-v3.21.4.tar.gz"
+  sha256_arm64 = "c06587fbc745538a1e8260b97c4f301ebf75d83565bcb3e16393c1f03f00c493"
 
   if Hardware::CPU.intel?
     sha256 sha256_x64
